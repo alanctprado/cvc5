c0f1d97d720329a98504fe35a39f936a8d4e7967
Alan Prado <alanctprado@gmail.com>
2024-05-06 20:42:25 -0300

    Refactor `pb_node_blaster.cpp`
    Everything uses Nodes now
    
    Signed-off-by: Alan Prado <alanctprado@gmail.com>

--- src/theory/bv/pb/pb_node_blaster.cpp
+++ src/theory/bv/pb/pb_node_blaster.cpp
@@ -15,13 +15,6 @@
 
 #include "theory/bv/pb/pb_node_blaster.h"
 
-#include "util/rational.h"
-
-#include <algorithm>
-#include <sstream>
-#include <string>
-#include <unordered_set>
-
 namespace cvc5::internal {
 namespace theory {
 namespace bv {
@@ -39,14 +32,14 @@ void PseudoBooleanBlaster::blastAtom(Node atom)
    * Note: We rewrite the nodes because it's not guaranteed (yet) that facts
    * sent to theories are rewritten.
    */
-  Trace("bv-pb-blast") << "Original atom: " << atom << "\n";
+  Trace("bv-pb") << "\nOriginal atom: " << atom << "\n";
   if (hasAtom(atom)) return;
 
   Node result;
   if (atom.getKind() == Kind::NOT)
   {
     Node normalized = rewrite(atom[0]);
-    Trace("bv-pb") << "Normalized atom: " << normalized << "; Kind: "
+    Trace("bv-pb-blast") << "\nNormalized atom: " << normalized << "; Kind: "
                    << normalized.getKind() << "\n";
     result = d_negAtomStrategies[
       static_cast<uint32_t>(normalized.getKind())
@@ -56,57 +49,21 @@ void PseudoBooleanBlaster::blastAtom(Node atom)
   else
   {
     Node normalized = rewrite(atom);
-    Trace("bv-pb") << "\nNormalized atom: " << normalized << "; Kind: "
+    Trace("bv-pb-blast") << "\nNormalized atom: " << normalized << "; Kind: "
                    << normalized.getKind() << "\n";
     result = d_atomStrategies[
       static_cast<uint32_t>(normalized.getKind())
     ](normalized, this);
   }
-  Trace("bv-pb-blast") << "Blasted atom:" << result << "\n";
-//  if (TraceIsOn("bv-pb-blast"))
-//  {
-//    for (std::string c : constraints) { Trace("bv-pb-blast") << c; }
-//  }
-//
-//  Subproblem unique_constraints;
-//  simplifyConstraints(constraints, unique_constraints);
-  //  I actually want to use a unordered_set when creating the atom
-//  storeAtom(atom, unique_constraints);
-}
 
-bool PseudoBooleanBlaster::hasAtom(Node atom) const
-{
-  return d_pbAtoms.find(atom) != d_pbAtoms.end();
-}
+  Trace("bv-pb-blast") << "Blasted atom:\n";
+  if (TraceIsOn("bv-pb-blast"))
+  {
+    for (Node c : result) { Trace("bv-pb-blast") << c << "\n"; }
+  }
 
-//void PseudoBooleanBlaster::simplifyConstraints(std::vector<std::string>
-//                                               constraints, Subproblem& ret)
-//{
-//  std::unordered_set<std::string> unique_constraints;
-//  std::unordered_set<unsigned> unique_variables;
-//  for (std::string c : constraints) { unique_constraints.insert(c); }
-//  for (std::string c : unique_constraints)
-//  {
-//    ret.second.push_back(c);
-//    size_t prev = 0;
-//    size_t next = 0;
-//    std::string token;
-//    while ((next = c.find(" ", prev)) != std::string::npos)
-//    {
-//      token = c.substr(prev, next - prev);
-//      if (token[0] == 'x')
-//        unique_variables.insert(std::stoul(token.substr(1)));
-//      prev = next + 1;
-//    }
-//  }
-//  for (unsigned v : unique_variables) { ret.first.push_back(v); }
-//}
-//
-//
-//void PseudoBooleanBlaster::storeAtom(TNode atom, Subproblem atom_bb)
-//{
-//  d_pbAtoms.emplace(atom, atom_bb);
-//}
+  storeAtom(atom, result);
+}
 
 Node PseudoBooleanBlaster::newVariable(unsigned numBits)
 {
@@ -126,7 +83,8 @@ Node PseudoBooleanBlaster::blastTerm(Node term)
   Assert(term.getType().isBitVector());
   if (hasTerm(term))
   {
-    Trace("bv-pb") << "Recovered bits " << getTerm(term)[0] << " for term " << term << "\n";
+    Trace("bv-pb") << "Recovered bits " << getTerm(term)[0] << " for term "
+                   << term << "\n";
     return getTerm(term);
   }
   Kind kind = term.getKind();
@@ -135,13 +93,6 @@ Node PseudoBooleanBlaster::blastTerm(Node term)
   storeTerm(term, result);
   return result;
 }
-
-//std::pair<std::vector<unsigned>, std::vector<std::string>>
-//PseudoBooleanBlaster::getStoredAtom(TNode atom)
-//{
-//  Assert(hasAtom(atom));
-//  return d_pbAtoms.at(atom);
-//}
 
 }  // namespace pb
 }  // namespace bv
