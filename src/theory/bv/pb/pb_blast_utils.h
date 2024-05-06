cbe9366c1829936e9329e507bd20de8bacd4d938
Alan Prado <alanctprado@gmail.com>
2024-05-06 14:03:06 -0300

    PB Xor between bit-vectors using nodes
    Useful to define the negation literal over bit-vectors.
    
    Signed-off-by: Alan Prado <alanctprado@gmail.com>

--- src/theory/bv/pb/pb_blast_utils.h
+++ src/theory/bv/pb/pb_blast_utils.h
@@ -36,6 +36,9 @@ template <class T> class TPseudoBooleanBlaster;
 template <class T> inline
 T mkConstraintNode(Kind k, std::vector<T> variables,
                    std::vector<T> coefficients, T value, NodeManager* nm);
+template <class T> inline
+T mkConstraintNode(Kind k, std::vector<T> variables,
+                   std::vector<int> coefficients, int value, NodeManager* nm);
 
 template <class T> inline
 T mkTermNode(T variables, std::vector<T> constraints, NodeManager* nm);
@@ -71,6 +74,20 @@ T mkConstraintNode(Kind k, std::vector<T> variables,
   T linear_form = size == 1 ? terms[0] : nm->mkNode(Kind::ADD, terms);
   T result = nm->mkNode(k, linear_form, value);
   return result;
+} 
+
+template <class T> inline
+T mkConstraintNode(Kind k, std::vector<T> variables,
+                   std::vector<int> coefficients, int value, NodeManager* nm)
+{
+  Assert(variables.size() == coefficients.size());
+  std::vector<T> coefficients_t;
+  for (int coeff : coefficients)
+  {
+    coefficients_t.push_back(nm->mkConstInt(Rational(coeff)));
+  }
+  T value_t = nm->mkConstInt(value);
+  return mkConstraintNode(k, variables, coefficients_t, value_t, nm);
 } 
 
 /**
@@ -118,7 +135,7 @@ T mkAtomNode(std::unordered_set<T> constraints, NodeManager* nm)
   v.reserve(constraints.size());
   for (auto it = constraints.begin(); it != constraints.end();)
   {
-      v.push_back(std::move(constraints.extract(it++).value()));
+    v.push_back(std::move(constraints.extract(it++).value()));
   } // I'm pretty sure this works...
   return mkAtomNode(v, nm);
 } 
@@ -178,23 +195,23 @@ std::vector<T> bvToUnsigned(unsigned size, NodeManager* nm, int sign)
 //  os <<  ">= 1 ;\n";
 //  return os.str();
 //} 
-//
-//template <class T> std::vector<std::string> mkPbXor(T a, T b, T res);
-//
-//template <class T> inline
-//std::vector<std::string> mkPbXor(T a, T b, T res)
-//{
-//  std::vector<std::string> constraints;
-//  constraints.push_back(mkPbVar(res, -1) + " " + mkPbVar(a, 1) + " "
-//                        + mkPbVar(b, 1) + " >= 0 ;\n");
-//  constraints.push_back(mkPbVar(res, -1) + " " + mkPbVar(a, -1) + " "
-//                        + mkPbVar(b, -1) + " >= -2 ;\n");
-//  constraints.push_back(mkPbVar(res, 1) + " " + mkPbVar(a, 1) + " "
-//                        + mkPbVar(b, -1) + " >= 0 ;\n");
-//  constraints.push_back(mkPbVar(res, 1) + " " + mkPbVar(a, -1) + " "
-//                        + mkPbVar(b, 1) + " >= 0 ;\n");
-//  return constraints;
-//}
+
+template <class T> std::vector<std::string> mkPbXor(T a, T b, T res);
+
+template <class T> inline
+std::vector<T> mkPbXor(T a, T b, T res, NodeManager* nm)
+{
+  std::vector<T> constraints;
+  constraints.push_back(mkConstraintNode(
+      Kind::GEQ, std::vector<T>{res, a, b}, std::vector<int>{-1, 1, 1}, 0, nm));
+  constraints.push_back(mkConstraintNode(
+      Kind::GEQ, std::vector<T>{res, a, b}, std::vector<int>{-1, -1, -1}, -2, nm));
+  constraints.push_back(mkConstraintNode(
+      Kind::GEQ, std::vector<T>{res, a, b}, std::vector<int>{1, 1, -1}, 0, nm));
+  constraints.push_back(mkConstraintNode(
+      Kind::GEQ, std::vector<T>{res, a, b}, std::vector<int>{1, -1, 1}, 0, nm));
+  return constraints;
+}
 
 template <class T> inline
 int ceil_log2(T a)
