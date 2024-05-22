09ccfa55d13dae4fadd01a14860d6e1c2c0b656e
Alan Prado <alanctprado@gmail.com>
2024-05-22 12:45:09 -0300

    Added 'Exact' options in 'configure.sh'
    Signed-off-by: Alan Prado <alanctprado@gmail.com>

--- configure.sh
+++ configure.sh
@@ -62,6 +62,7 @@ The following flags enable optional packages (disable with --no-<option name>).
   --kissat                 use the Kissat SAT solver
   --poly                   use the LibPoly library [default=yes]
   --cocoa                  use the CoCoA library
+  --exact                  use the Exact Pseudo-Boolean solver
   --editline               support the editline library
 
 Optional Path to Optional Packages:
@@ -120,6 +121,7 @@ gpl=default
 kissat=default
 poly=ON
 cocoa=default
+exact=default
 muzzle=default
 ninja=default
 profiling=default
@@ -252,6 +254,9 @@ do
 
     --cocoa) cocoa=ON;;
     --no-cocoa) cocoa=OFF;;
+
+    --exact) exact=ON;;
+    --no-exact) exact=OFF;;
 
     --muzzle) muzzle=ON;;
     --no-muzzle) muzzle=OFF;;
@@ -418,6 +423,8 @@ fi
   && cmake_opts="$cmake_opts -DUSE_POLY=$poly"
 [ $cocoa != default ] \
   && cmake_opts="$cmake_opts -DUSE_COCOA=$cocoa"
+[ $exact != default ] \
+  && cmake_opts="$cmake_opts -DUSE_EXACT=$exact"
 [ "$glpk_dir" != default ] \
   && cmake_opts="$cmake_opts -DGLPK_DIR=$glpk_dir"
 [ "$dep_path" != default ] \
