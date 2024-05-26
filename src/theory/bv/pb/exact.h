8559da12b1f92a49f839abec52f1ab6b395c5a2b
Alan Prado <alanctprado@gmail.com>
2024-05-26 03:16:33 -0300

    Initial implementation of 'Exact' interface
    Signed-off-by: Alan Prado <alanctprado@gmail.com>

---
+++ src/theory/bv/pb/exact.h
@@ -0,0 +1,89 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Aina Niemetz, Mathias Preiner, Andrew Reynolds
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2024 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Wrapper for CaDiCaL SAT Solver.
+ *
+ * Implementation of the CaDiCaL SAT solver for cvc5 (bit-vectors).
+ */
+
+#include "cvc5_private.h"
+
+#ifndef CVC5__THEORY__BV__PB__EXACT_H
+#define CVC5__THEORY__BV__PB__EXACT_H
+
+#include <Exact-EP/src/Exact.hpp>
+
+//#include "context/cdhashset.h"
+#include "theory/bv/pb/pb_solver.h"
+#include "smt/env_obj.h"
+
+namespace cvc5::internal {
+namespace theory {
+namespace bv {
+namespace pb {
+
+//class CadicalPropagator;
+
+class ExactSolver : public PbSolver<Node>, protected EnvObj
+{
+  // friend class SatSolverFactory;
+
+ public:
+  ~ExactSolver() override = default;
+
+  /* ExactSolver interface -------------------------------------------------- */
+  void addConstraint(Node) override;
+  void addVariable(Node) override;
+  PbValue solve() override;
+
+// private:   TODO: constructor should be private (factory)
+  /**
+   * Constructor.
+   * Private to disallow creation outside of SatSolverFactory.
+   * Function init() must be called after creation.
+   * @param env       The associated environment.
+   * @param registry  The associated statistics registry.
+   * @param name      The name of the SAT solver.
+   * @param logProofs Whether to log proofs
+   */
+  ExactSolver(Env& env,
+              StatisticsRegistry& registry,
+              const std::string& name = "",
+              bool logProofs = false);
+ private:
+
+  /**
+   * Initialize PB solver instance.
+   * Note: Split out to not call virtual functions in constructor.
+   */
+  void init();
+
+  /** The wrapped Exact instance. */
+  std::unique_ptr<Exact> d_solver;
+
+  /** Whether we are logging proofs */
+  bool d_logProofs;
+
+  /** TODO: implement statistics */
+  struct Statistics
+  {
+    Statistics(StatisticsRegistry& registry, const std::string& prefix);
+  };
+  Statistics d_statistics;
+};
+
+}  // namespace pb
+}  // namespace bv
+}  // namespace theory
+}  // namespace cvc5::internal
+
+#endif  // CVC5__THEORY__BV__PB__EXACT_H
